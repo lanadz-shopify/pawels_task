53350ed51f9009ee069885fc69d37ba861e41bf8
Svitlana Dzyban <svitlana.dzyuban@gmail.com>
2015-06-04 16:36:12 +0300

    Refactored code

--- text_evaluator.rb
+++ text_evaluator.rb
@@ -4,55 +4,66 @@
 #
 # and outputs the word which is used the most frequently
 
-class Counter
-
+class TextEvaluator
   def initialize(text)
     @text = text
+    split_words
   end
 
   def most_frequent
-    words_ary = text.split(' ')
-    frequent_word = ""
-    max_count = 0
-
-    words_ary.each do |word|
-      frequent_word = word if max_count < count_word(word)
-    end
-
-    frequent_word
+    result = group_words_by_count.max_by{|_, count| count} #returns array [element, count]
+    result.nil? ? ""  : result[0]
   end
 
-  def group_by_word
-
+  def group_words_by_count
+    split_words.inject({}) do |memo, word|
+      if memo[word]
+        memo[word] += 1
+      else
+        memo[word] = 1
+      end
+      memo
+    end
   end
 
   private
 
-  attr_reader :text
+  attr_reader :text, :words_array
 
-  def count_word(word)
-    words_ary = text.split(' ')
-    result = 0
-    words_ary.each do |element|
-      if element == word
-        result += 1
-      end
-    end
-    result
+  def split_words
+    @words_array ||= text.split(' ')  #lazy loading
   end
-
 end
 
-describe Counter do
+describe TextEvaluator do
+  let(:input_text){"word1 word2 word3 word2 word3 word4 word3 word4 word4 word4"}
+
   describe "#most_frequent" do
     it "finds the most frequent word" do
-      input_text = "outputs the word which is used the most frequently word"
       obj = described_class.new(input_text)
-      expect(obj.most_frequent).to eq("word")
+      expect(obj.most_frequent).to eq("word4")
     end
 
-    xit "returns empty string for empty text" do
+    it "returns empty string for empty text" do
+      obj = described_class.new("")
+      expect(obj.most_frequent).to eq("")
+    end
+  end
 
+  describe "#group_words_by_count" do
+    it "counts all words and write it as hash" do
+      obj = described_class.new(input_text)
+      expect(obj.group_words_by_count).to eq({
+                                                 "word1" => 1,
+                                                 "word2" => 2,
+                                                 "word3" => 3,
+                                                 "word4" => 4,
+                                             })
+    end
+
+    it "returns empty hash if no text is given" do
+      obj = described_class.new("")
+      expect(obj.group_words_by_count).to eq({})
     end
   end
 end
